70f85f685ad0832147528785741f8d82f2cf4f9d
fernandaSolc <fernandasabrina.8272@gmail.com>
2024-06-26 17:29:25 -0300

    json com problemas

--- horarioBio/src/components/tabela/Tabela.jsx
+++ horarioBio/src/components/tabela/Tabela.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Tabela.css'
 import busca from '../../assets/iconBusca.svg'
+import '../../../server/data.json'
 
 // Componente para renderizar a tabela
 const Tabela = () => {
   const [dados, setDados] = useState([]);
   const [horaFilterOpen, setHoraFilterOpen] = useState(false);
   const [dataFilterOpen, setDataFilterOpen] = useState(false);
+  const [pesquisa, setpesquisa] = useState();
+  const funcionariosFiltrados = Funcionarios.filter((nome) => nome.startsWith(pesquisa));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,9 +44,15 @@ const Tabela = () => {
         <thead>
           <tr className='titulos1'>
           <div className='busca'>
+            value={pesquisa}
             <img className='icon' src={busca}></img> 
-          <input className='pesquisa' placeholder='Search'></input>
-          </div>
+          <input className='pesquisa' onChange={(ev) => setpesquisa(ev.target.value)} placeholder='Search'></input>
+          {funcionariosFiltrados.map((nome)=>
+          (
+            <li key={nome}>{nome}</li>
+          )
+          )}
+           </div>
            <th className='datas'>Hora</th>
            <button className="filter-button" onClick={toggleHoraFilter}>
                 <span className={`arrow ${horaFilterOpen ? 'open' : ''}`}>&#9660;</span>
